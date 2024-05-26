9978c6483f5975278acd58eb34a4695c9b218a4f
Ashish Jain <imjainashish@gmail.com>
2024-05-26 15:14:45 +0530

    Initial Commit

---
+++ MetalTrySwiftUI/MetalTrySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalTrySwiftUIApp.swift
+//  MetalTrySwiftUI
+//
+//  Created by Ashish Jain on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalTrySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
